27e79fb513178ddce89e5c68f342b78e157c2eea
westkitekim <westkite418@gmail.com>
2022-03-23 22:58:47 +0900

    Baekjoon | automata3 - B2557_Hello World

---
+++ baekjoon/baekjoon_automata_basic/B2557_HelloWorld.java
@@ -0,0 +1,12 @@
+package baekjoon_automata_basic;
+
+/**
+ * Baekjoon 2557 - Hello World
+ * Bronze_5
+ * automata_1
+ */
+public class B2557_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
