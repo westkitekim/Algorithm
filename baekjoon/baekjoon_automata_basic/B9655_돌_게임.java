9188422ab54ceca0145a3af472a09e319230f8a3
westkitekim <westkite418@gmail.com>
2022-05-01 22:07:47 +0900

    Baekjoon | Automata Basic - B9655_돌_게임

---
+++ baekjoon/baekjoon_automata_basic/B9655_돌_게임.java
@@ -0,0 +1,14 @@
+package baekjoon_automata_basic;
+
+import java.util.Scanner;
+
+public class B9655_돌_게임 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if(N % 2 == 0) System.out.println("CY");
+        else System.out.println("SK");
+    }
+}
