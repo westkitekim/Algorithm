ca25d4b464a50c458db3d1579488aa1ca5867bd4
westkitekim <westkite418@gmail.com>
2022-06-03 21:23:34 +0900

    Baekjoon | Automata Basic - B2558_ABsum_2.java

---
+++ baekjoon/baekjoon_automata_basic/B2558_ABsum_2.java
@@ -0,0 +1,14 @@
+package baekjoon_automata_basic;
+
+import java.util.Scanner;
+
+public class B2558_ABsum_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+    }
+}
