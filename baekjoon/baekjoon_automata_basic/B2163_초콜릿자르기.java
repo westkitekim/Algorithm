476f1c688e153be347088c540f82518d59dfc3cb
westkitekim <westkite418@gmail.com>
2022-06-19 06:49:12 +0900

    Baekjoon | Automata Basic - B2163_초콜릿자르기

---
+++ baekjoon/baekjoon_automata_basic/B2163_초콜릿자르기.java
@@ -0,0 +1,21 @@
+package baekjoon_automata_basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 이게 왜... 맞음..>?;;
+ */
+public class B2163_초콜릿자르기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println((M * N) - 1);
+    }
+}
